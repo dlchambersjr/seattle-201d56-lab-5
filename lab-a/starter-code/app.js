00bb506b081633dc60af7ad2760bb8f677baed51
David Chambers <dlchambersjr@gmail.com>
2019-03-29 11:43:13 -0700

    solves problem-1

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -9,11 +9,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  // array = [total, string];
+
+  var output = [];
+  var total = a + b;
+  output.push(total);
+
+  output.push('The sum of ' + a + ' and ' + b + ' is ' + total + '.');
+
+
+  return output
+
 
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -116,7 +127,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
